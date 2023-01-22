4e5b3cfa5eaaaf3cc2ce1a7637e90d17a1811936
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-01-22 17:04:15 +0000

    add a preview recipe list

--- bin-times/src/App.jsx
+++ bin-times/src/App.jsx
@@ -1,5 +1,5 @@
 import { container, containerBody, containerHeader, containerH1, nav } from "./App.module.css"
-import { newRecipe } from "./App.module.css"
+import { newRecipe, recipeList, recipe } from "./App.module.css"
 import { TbCarrot } from 'solid-icons/tb'
 
 function Nav() {
@@ -11,6 +11,17 @@ function Nav() {
   );
 }
 
+function RecipeList() {
+  return (
+    <div class={ recipeList }>
+      <div class={ recipe }>Mon</div>
+      <div class={ recipe }>Tue</div>
+      <div class={ recipe }>Wed</div>
+      <div class={ recipe }>Thurs</div>
+      <div class={ recipe }>Fri</div>
+    </div>
+  )
+}
 
 function NewRecipe() {
   const onClick = () => {
@@ -24,6 +35,7 @@ function App() {
     <div class={ container }>
       <Nav />
       <div class={ containerBody }>
+        <RecipeList />
         <header class={ containerHeader }>
           <h1 class={ containerH1 }>Today's recipe</h1>
         </header>
