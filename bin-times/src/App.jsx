7261c0e658c689d000ec514604f4159c304254ca
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-01-22 16:29:57 +0000

    recipe app

--- bin-times/src/App.jsx
+++ bin-times/src/App.jsx
@@ -1,11 +1,26 @@
-import { container, containerHeader, containerH1 } from "./App.module.css"
+import { container, containerBody, containerHeader, containerH1, nav } from "./App.module.css"
+import { newRecipe } from "./App.module.css"
+import { TbCarrot } from 'solid-icons/tb'
+
+function Nav() {
+  return <div class={ nav }>Nav</div>
+}
+
+
+function NewRecipe() {
+  return <div class={ newRecipe }><TbCarrot /><div>Add recipe</div></div>
+}
 
 function App() {
   return (
     <div class={ container }>
-      <header class={ containerHeader }>
-        <h1 class={ containerH1 }>Web design</h1>
-      </header>
+      <Nav />
+      <div class={ containerBody }>
+        <header class={ containerHeader }>
+          <h1 class={ containerH1 }>Recipe finder</h1>
+        </header>
+        <NewRecipe />
+      </div>
     </div>
   );
 }
