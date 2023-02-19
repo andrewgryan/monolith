52064aeb0b6b1c1f31bd27cddb46589febb02aec
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-19 19:56:05 +0000

    add an example typescript project

---
+++ side-project/src/index.ts
@@ -0,0 +1,17 @@
+interface Point {
+  x: number;
+  y: number;
+}
+
+const add = (p: Point): number => {
+  return p.x + p.y;
+};
+
+export default function HelloWorld(): number {
+  return add({ x: 1, y: 1 });
+}
+
+let el = document.getElementById("app");
+if (el != null) {
+  el.appendChild(document.createTextNode("VanillaJS"));
+}
