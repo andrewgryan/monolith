49bd58cd91b2f082471c1662550e7905f6a1fe69
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-20 10:57:46 +0000

    move html to solid app

--- side-project/src/App.tsx
+++ side-project/src/App.tsx
@@ -1,5 +1,11 @@
 function Vanilla() {
-  return <div>VanillaJS</div>;
+  return (
+    <main class="grid place-items-center bg-gray-100 min-h-screen">
+      <header>
+        <h1 class="text-3xl font-bold underline">Solid JS!</h1>
+      </header>
+    </main>
+  );
 }
 
 export default Vanilla;
