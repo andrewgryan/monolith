d17397e2fb16b58486e36bf4e7821e6b20a78a56
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-20 11:10:27 +0000

    add supabase data to app

--- side-project/src/App.tsx
+++ side-project/src/App.tsx
@@ -1,11 +1,31 @@
-function Vanilla() {
+import { createSignal, createEffect, For } from "solid-js";
+import { createClient } from "@supabase/supabase-js";
+// Supabase client
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+function App() {
+  const [countries, setCountries] = createSignal([]);
+
+  createEffect(() => {
+    getCountries();
+  });
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
   return (
     <main class="grid place-items-center bg-gray-100 min-h-screen">
       <header>
         <h1 class="text-3xl font-bold underline">Solid JS!</h1>
+        <ul>
+          <For each={countries()}>{(country) => <li>{country.name}</li>}</For>
+        </ul>
       </header>
     </main>
   );
 }
 
-export default Vanilla;
+export default App;
--- side-project/src/index.tsx
+++ side-project/src/index.tsx
@@ -1,20 +1,9 @@
 import "./index.css";
-import { createClient } from "@supabase/supabase-js";
 import { render } from "solid-js/web";
-import Vanilla from "./App";
-
-// Supabase client
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-supabase
-  .from("countries")
-  .select()
-  .then(({ data }) => console.log(data));
+import App from "./App";
 
 // Solid app
 let el = document.getElementById("app");
 if (el != null) {
-  render(() => <Vanilla />, el);
+  render(() => <App />, el);
 }
