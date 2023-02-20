bc90c2a72034ea9abf848044c9bbea2efb129dbc
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-20 15:30:20 +0000

    tweak forms

--- side-project/src/App.tsx
+++ side-project/src/App.tsx
@@ -1,4 +1,5 @@
-import { Show, For, createResource } from "solid-js";
+import { createResource, createSignal, createEffect } from "solid-js";
+import { createStore } from "solid-js/store";
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/supabase";
 
@@ -8,8 +9,14 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 function App() {
+  const [store, setStore] = createStore({ name: "", description: "" });
+
+  createEffect(() => {
+    console.log(store.name, store.description);
+  });
+
   return (
-    <main class="grid place-items-center text-indigo-900 bg-gradient-to-b from-indigo-400 via-purple-400 to-pink-400 min-h-screen">
+    <main class="grid place-items-center text-indigo-900 bg-gradient-to-b from-purple-500 via-purple-400 to-purple-500 min-h-screen">
       <header class="flex flex-col space-y-8">
         <h1 class="text-3xl font-bold uppercase tracking-wide">
           Add an ingredient
@@ -24,7 +31,8 @@ function App() {
             </label>
             <input
               id="ingredient"
-              class="appearence-none bg-indigo-200 text-indigo-700 focus:bg-white py-2 px-4 leading-tight"
+              oninput={(ev) => setStore("name", ev.currentTarget.value)}
+              class="appearence-none bg-purple-200 focus:outline-purple-700 text-purple-700 focus:bg-white py-2 px-4 leading-tight"
               type="text"
             />
           </div>
@@ -37,7 +45,8 @@ function App() {
             </label>
             <input
               id="ingredient-description"
-              class="appearence-none bg-indigo-200 text-indigo-700 focus:bg-white py-2 px-4 leading-tight"
+              oninput={(ev) => setStore("description", ev.currentTarget.value)}
+              class="appearence-none bg-purple-200 focus:outline-purple-700 text-purple-700 focus:bg-white py-2 px-4 leading-tight"
               type="text"
             />
           </div>
@@ -50,7 +59,18 @@ function App() {
             </button>
             <button
               type="button"
-              class="uppercase font-semibold tracking-wide bg-pink-500 text-pink-100 py-2"
+              disabled={store.name === ""}
+              class={
+                store.name === ""
+                  ? "uppercase font-semibold tracking-wide bg-pink-300 text-pink-100 py-2"
+                  : "uppercase font-semibold tracking-wide bg-pink-500 text-pink-100 py-2"
+              }
+              onclick={() =>
+                console.log({
+                  name: store.name,
+                  description: store.description,
+                })
+              }
             >
               Add
             </button>
