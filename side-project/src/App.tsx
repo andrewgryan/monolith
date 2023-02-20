5db7e9274ecd6933473b510f2d2f5ad6e9e547a2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-20 14:38:44 +0000

    simplify form

--- side-project/src/App.tsx
+++ side-project/src/App.tsx
@@ -7,44 +7,53 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-type Country = {
-  name: string;
-};
-
-async function fetchCountries() {
-  const { data } = await supabase.from("countries").select();
-  if (data != null) {
-    return data;
-  } else {
-    return [];
-  }
-}
-
 function App() {
-  const [countries] = createResource<Country[]>(fetchCountries);
-
   return (
-    <main class="grid place-items-center text-gray-800 bg-gradient-to-b from-indigo-400 via-purple-400 to-pink-400 min-h-screen">
-      <header class="flex flex-col space-y-2">
-        <h1 class="text-3xl font-bold">Recipe</h1>
-        <Show when={!countries.loading} fallback={<div>...</div>}>
-          <ul>
-            <For each={countries()}>{(country) => <li>{country.name}</li>}</For>
-          </ul>
-        </Show>
-        <form>
+    <main class="grid place-items-center text-indigo-900 bg-gradient-to-b from-indigo-400 via-purple-400 to-pink-400 min-h-screen">
+      <header class="flex flex-col space-y-8">
+        <h1 class="text-3xl font-bold uppercase tracking-wide">
+          Add an ingredient
+        </h1>
+        <form class="text-indigo-900 flex flex-col space-y-3">
           <div class="flex flex-col space-y-1">
             <label
               for="ingredient"
               class="uppercase tracking-wide font-semibold"
             >
-              Ingredient
+              Ingredient name
             </label>
             <input
               id="ingredient"
-              class="appearence-none bg-gray-200 text-gray-700 focus:bg-white py-2 px-4 leading-tight"
+              class="appearence-none bg-indigo-200 text-indigo-700 focus:bg-white py-2 px-4 leading-tight"
               type="text"
             />
+          </div>
+          <div class="flex flex-col space-y-1">
+            <label
+              for="ingredient-description"
+              class="uppercase tracking-wide font-semibold"
+            >
+              Description
+            </label>
+            <input
+              id="ingredient-description"
+              class="appearence-none bg-indigo-200 text-indigo-700 focus:bg-white py-2 px-4 leading-tight"
+              type="text"
+            />
+          </div>
+          <div class="grid grid-cols-2 gap-2">
+            <button
+              type="button"
+              class="uppercase tracking-wide text-indigo-600 bg-white py-2"
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              class="uppercase font-semibold tracking-wide bg-pink-500 text-pink-100 py-2"
+            >
+              Add
+            </button>
           </div>
         </form>
       </header>
