ab24005507181201085e453d171f10d274c03941
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-23 18:31:28 +0000

    refactoring

--- side-project/src/Blog.tsx
+++ side-project/src/Blog.tsx
@@ -87,7 +87,16 @@ export function Post() {
 }
 
 export function NewPost() {
-  return <EditPost title="" description="" />;
+  return (
+    <div class="bg-gray-800 text-white h-screen">
+      <header class="grid place-items-center">
+        <h1 class="text-3xl tracking-wide font-thin uppercase pt-6">
+          New post
+        </h1>
+      </header>
+      <EditPost title="" description="" />
+    </div>
+  );
 }
 
 export const EditPost: Component<{ title: string; description: string }> = (
@@ -110,44 +119,37 @@ export const EditPost: Component<{ title: string; description: string }> = (
     ]);
   };
   return (
-    <div class="bg-gray-800 text-white h-screen">
-      <header class="grid place-items-center">
-        <h1 class="text-3xl tracking-wide font-thin uppercase pt-6">
-          New post
-        </h1>
-      </header>
-      <form class="mx-auto max-w-lg grid gap-4 pt-12 px-2">
-        <div class="grid">
-          <label for="title">Title</label>
-          <input
-            id="title"
-            type="text"
-            class="bg-gray-700 rounded-md"
-            value={title()}
-            oninput={(ev) => setTitle(ev.currentTarget.value)}
-          />
-        </div>
-        <div class="grid">
-          <label for="description">Description</label>
-          <textarea
-            id="description"
-            class="bg-gray-700 resize-y rounded-md"
-            value={description()}
-            oninput={(ev) => setDescription(ev.currentTarget.value)}
-          />
-        </div>
-        <div class="grid">
-          <button
-            type="button"
-            onclick={onSubmit}
-            class="text-xl uppercase tracking-wide bg-pink-500 text-pink-100 rounded-md py-2 shadow shadow-pink-500/50"
-            disabled={title() === ""}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+    <form class="mx-auto max-w-lg grid gap-4 pt-12 px-2">
+      <div class="grid">
+        <label for="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          class="bg-gray-700 rounded-md"
+          value={title()}
+          oninput={(ev) => setTitle(ev.currentTarget.value)}
+        />
+      </div>
+      <div class="grid">
+        <label for="description">Description</label>
+        <textarea
+          id="description"
+          class="bg-gray-700 resize-y rounded-md"
+          value={description()}
+          oninput={(ev) => setDescription(ev.currentTarget.value)}
+        />
+      </div>
+      <div class="grid">
+        <button
+          type="button"
+          onclick={onSubmit}
+          class="text-xl uppercase tracking-wide bg-pink-500 text-pink-100 rounded-md py-2 shadow shadow-pink-500/50"
+          disabled={title() === ""}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   );
 };
 
