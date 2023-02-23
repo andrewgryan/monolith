fc7cb94d4764fc30ff82a8077c53ddd6847e3e0f
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-02-23 10:05:29 +0000

    more readable post

--- side-project/src/Blog.tsx
+++ side-project/src/Blog.tsx
@@ -2,7 +2,15 @@ import { useParams } from "@solidjs/router";
 
 export function Post() {
   const params = useParams();
-  return <div>Post: {params.id}</div>;
+  return (
+    <div class="bg-gray-800 text-white h-screen">
+      <article class="mx-auto max-w-lg pt-12">
+        <header class="text-xl uppercase tracking-wide text-pink-500">
+          Post: {params.id}
+        </header>
+      </article>
+    </div>
+  );
 }
 
 export default function Blog() {
