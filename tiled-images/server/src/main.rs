3d0b028ec28c4f37c00af9772e86601c14f60bbf
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-05-10 14:10:04 +0100

    put server into directory

--- tiled-images/server/src/main.rs
+++ tiled-images/server/src/main.rs
@@ -1,5 +1,5 @@
 use actix_cors::Cors;
-use actix_web::{get, http, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, http, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::io::Cursor;
 
 struct AppState {
@@ -7,10 +7,27 @@ struct AppState {
 }
 
 #[get("/{z}/{x}/{y}.jpg")]
-async fn cat(data: web::Data<AppState>) -> impl Responder {
-    let dynamic_image = data
-        .dynamic_image
-        .resize(32, 32, image::imageops::FilterType::Triangle);
+async fn cat(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    // Parse URL
+    let z: u32 = req.match_info().get("z").unwrap().parse().unwrap();
+    let x: u32 = req.match_info().get("x").unwrap().parse().unwrap();
+    let y: u32 = req.match_info().get("y").unwrap().parse().unwrap();
+
+    // Estimate crop location
+    let xp: u32;
+    let yp: u32;
+    if z > 1 {
+        xp = x / (u32::pow(2, z - 1));
+        yp = y / (u32::pow(2, z - 1));
+    } else {
+        xp = x;
+        yp = y;
+    }
+    let i: u32 = xp * 32;
+    let j: u32 = yp * 32;
+
+    // Crop image
+    let dynamic_image = data.dynamic_image.crop_imm(i, j, 32, 32);
 
     let mut body: Vec<u8> = Vec::new();
     dynamic_image
@@ -20,10 +37,6 @@ async fn cat(data: web::Data<AppState>) -> impl Responder {
         )
         .expect("");
 
-    // let _image_content: Vec<u8> = web::block(|| std::fs::read("cat.jpg"))
-    //     .await
-    //     .unwrap()
-    //     .unwrap();
     HttpResponse::Ok().content_type("image/jpeg").body(body)
 }
 
@@ -40,8 +53,8 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors)
             .app_data(web::Data::new(AppState {
                 dynamic_image: image::open("cat.jpg").expect("").resize(
-                    32,
-                    32,
+                    64,
+                    64,
                     image::imageops::FilterType::Triangle,
                 ),
             }))
